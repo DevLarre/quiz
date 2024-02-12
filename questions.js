b017363d82948872b4945a4d31e64a178fe96cd1
Douglas Larré <120336094+DevLarre@users.noreply.github.com>
2024-02-12 20:51:59 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      question: "O que é HTML?",
+      answers: [
+        { option: "Uma Linguagem de programação.", correct: false },
+        { option: "Uma ferramenta de desenho.", correct: false },
+        { option: "Uma linguagem de marcação.", correct: true },
+        { option: "Um app de stremer.", correct: false },
+      ],
+    },
+    {
+      question: "A hitória do SO (Sistema Operacional), começa com o telégrafo, em que ano nasceu o primeiro Telégrafo?",
+      answers: [
+        { option: "1835", correct: true },
+        { option: "1870", correct: false },
+        { option: "1901", correct: false },
+        { option: "1905", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano surgiu a linguagem de PHP?",
+      answers: [
+        { option: "1990", correct: false },
+        { option: "1987", correct: false },
+        { option: "1997", correct: false },
+        { option: "1993", correct: true },
+      ],
+    },
+    {
+      question: "O sistema Linux é mundialmente conhecido em que linguagem ele foi programado?",
+      answers: [
+        { option: "Java Scrypt", correct: false },
+        { option: "C", correct: true },
+        { option: "C++", correct: true },
+        { option: "PHP", correct: true },
+      ],
+    },
+  ];
